af749af3ab61a0477f654cf011de16e638f0b959
abdullahshafiq153 <abdullahshafiq153@gmail.com>
2024-07-25 04:02:19 +0500

    more instructions

---
+++ 2-React, Props, State/todoApp1.js
@@ -0,0 +1,26 @@
+const list = document.getElementById("todo-list");
+const itemCountspan = document.getElementById("item-count");
+const uncheckedCountspan = document.getElementById("unchecked-count");
+
+// <li>
+//    <input type="checkbox"/>
+//    <button>delete</button>
+//    <span>text</span>
+//</li>
+function newTodo() {
+  //get text
+  //invoke createtodo()
+  //update counts
+  //append to list
+}
+function createTodo() {
+  //make li
+  //make Input
+  //make button
+  //make span
+}
+function deleteTodo() {
+  //find the todo to delete
+  //delete
+  //update the counts
+}
